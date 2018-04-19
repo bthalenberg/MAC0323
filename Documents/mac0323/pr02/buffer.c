c42e30ede6b55330879fe43bd7b077bccdb87a0b
Desktop <gui.yambanis@gmail.com>
2018-04-19 15:30:43 -0300

    Makefile, center.c com os includes e que le os argumentos corretos da linha de comando e tentativa de inicio de buffer.c

---
+++ Documents/mac0323/pr02/buffer.c
@@ -0,0 +1,27 @@
+#include "buffer.h"
+
+/*
+  Create and return a new buffer that holds member elements of size
+  member_size.
+*/
+Buffer *buffer_create(size_t member_size){
+	Buffer *B = malloc(sizeof(Buffer));
+	B->member_size = member_size;
+	B->buffer_size = 1;
+	B->p = 0;
+	return B;
+}
+
+/*
+  Destroy a buffer.
+*/
+void buffer_destroy(Buffer *B){
+
+}
+
+/*
+  Reset buffer, eliminating contents.
+*/
+void buffer_reset(Buffer *B){
+	*B = *buffer_create(B->member_size);
+}
--- Documents/mac0323/pr02/center.c
+++ Documents/mac0323/pr02/center.c
@@ -1,1 +1,25 @@
 #include <ctype.h>
+#include <stdio.h>
+#include "buffer.h"
+
+int main(int argc, char *argv[])
+{	
+	char *input, *output;
+	size_t member_size;
+	Buffer *B;
+
+	//nome do arquivo de entrada
+	input = argv[1];
+
+	//nome do arquivo de saida
+	output = argv[2];
+
+	//numero de colunas c
+	member_size = atoi(argv[3]);
+
+	printf("%s\n", input);
+	printf("%s\n", output);
+	printf("%ld\n", member_size);
+
+	return 0;
+}
