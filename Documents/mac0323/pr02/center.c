8535960856df60c49f7d83630e1fd8f6d72adb83
Desktop <gui.yambanis@gmail.com>
2018-04-24 18:51:22 -0300

    Adiciona impressao de erro em linhas muito longas

--- Documents/mac0323/pr02/center.c
+++ Documents/mac0323/pr02/center.c
@@ -18,9 +18,16 @@ int main(int argc, char *argv[])
 	Buffer *B = buffer_create(member_size);
 
 	int i = 1;
+	int line = 0;
+	int errorLines[100];
+	int errors = 0;
 	do{
+		line++;
 		i = read_line(input, B);
 		int dif = B->member_size - B->buffer_size;
+		if(dif < 0) {
+			errorLines[errors++] = line;
+		}
 		for(int j = 1; j <= dif/2; j++) printf("_");
 		for(int j = 0; j < B->buffer_size-1; j++){
 			if(B->data[j] != EOF) printf("%c", B->data[j]);
@@ -29,6 +36,10 @@ int main(int argc, char *argv[])
 		printf("\n");
 
 	}while(i != 0);
+	
+	for(i = 0; i < errors; i++) 
+		fprintf(stderr, "center: %s: line %d: line too long.\n", argv[1], errorLines[i]);
+
 	
 	printf("\n\n");
 	//while(fgets(buffer, 999, input) != NULL) 
