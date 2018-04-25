2fdb79c2fc0075a217bab848f68e7a12098e0abe
Bruna Thalenberg <bthalenberg@gmail.com>
2018-04-25 10:31:40 -0300

    center removes useless blank lines; special cases for even and odd lengths

--- Documents/mac0323/pr02/center.c
+++ Documents/mac0323/pr02/center.c
@@ -20,20 +20,25 @@ int main(int argc, char *argv[])
 	int line = 0;
 	int errorLines[100];
 	int errors = 0;
+    int blankLines = 0;
 	do {
 		line++;
 		i = read_line(input, B);
         if (i == 0) break;
-		int dif = atoi(argv[3]) - i;
-		if (dif < 0) {
-			errorLines[errors++] = line;
-		}
-		for (int j = 0; j <= dif/2; j++) fprintf(output, "+");
-		for (unsigned j = 0; j < B->buffer_size-1; j++)
-			if(B->data[j] != EOF) fprintf(output, "%c", B->data[j]);
-		for (int j = 1; j <= dif/2; j++) fprintf(output, "+");
-		fprintf(output, "\n");
-
+        if (i == 1) {
+            blankLines++;
+        }
+        else blankLines = 0;
+        if (blankLines <= 1) {
+    		int dif = atoi(argv[3]) - (i - 1);
+    		if (dif < 0) {
+    			errorLines[errors++] = line;
+    		}
+            if (dif % 2 != 0) dif++;
+    		for (int j = 0; j < dif/2; j++) fprintf(output, "+");
+    		for (unsigned j = 0; j < B->buffer_size-1; j++)
+    			if(B->data[j] != EOF) fprintf(output, "%c", B->data[j]);
+        }
 	} while (i != 0);
 
 	for (i = 0; i < errors; i++)
