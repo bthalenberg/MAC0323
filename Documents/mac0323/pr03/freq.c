6209007d94cd77d8060cd3f245846adcd45a19c1
Bruna Thalenberg <bthalenberg@gmail.com>
2018-05-06 21:26:47 -0300

    Initial files

---
+++ Documents/mac0323/pr03/freq.c
@@ -0,0 +1,9 @@
+#include "stable.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) return 1;
+	//nome do arquivo de entrada
+	FILE *input = fopen(argv[1], "r");
+}
