5e6c0febac3d3d86a8e4f79cf4c6f9f6f64673a4
Bruna Thalenberg <bthalenberg@gmail.com>
2018-05-29 16:24:10 -0300

    assingment function draft

--- pr04/parse_test.c
+++ pr04/parse_test.c
@@ -26,9 +26,44 @@ void print_error(const char *errptr, Buffer *b, int line) {
     printf("^\n%s\n", error_msg);
 }
 
+/*
+  Reads a line in search for assignment operador IS
+  If not found, returns 0;
+  If found, tries to register it in the symbol table.
+  If value was already assigned, prints error message and returns 0.
+  Else, registers it and returns 1;
+*/
+int assignment_function(char *s, int i) {
+    int i = 0;
+    Buffer *b = buffer_create();
+    // Ignores spaces
+    while (isspace(s[i])) i++;
+    // Stops if first word is comment
+    if (s[i] == '*') return 0;
+    // Scans in search for IS
+    while (s[i] != '\0' && s[i] != '\n' && s[i] == 'I')
+        if (!isspace(s[i]) buffer_push_back (b, s[i++]);
+    if (s[i] == '\0' || s[i] == '\n') return 0;
+    else if (s[i] == 'S') {
+//         char *label = read_label(b);
+//         InsertionResult res = stable_insert(st, label);
+//         if (res.new == 0) {
+//             fprintf(stderr, "line     = %s\n", b->data);
+//             fprintf(stderr, "Invalid assignment: %s is already assigned.\n", label);
+//             return 0;
+
+//         }
+//         else {
+//             OP value = read_value();
+//             res->data = value;
+//         }
+    }
+    return 1;
+}
+
 int main(int argc, char* argv) {
     if (argc != 2) die("Invalid argument.\n");
-
+    // config for error.c
     set_prog_name("parse_test");
 
     SymbolTable *st = stable_create();
@@ -46,9 +81,9 @@ int main(int argc, char* argv) {
         if (len == 0) break;
         // quando a instrução é o pseudo-operador IS, associar o valor do operando ao rótulo passado
         // inserindo o par correspondente na alias_table. Se já foi associado, produzir mensagem de erro.
-
+        if (assignment_function()) continue;
         // if parse was successful, prints line and instr content
-        if (parse (b->data, st, &instr, &errptr)) {
+        else if (parse (b->data, st, &instr, &errptr)) {
             if (b->data[0] != '*') printf ("line     = %s\n", b->data);
             // aqui vem código para printar instruções
         }
