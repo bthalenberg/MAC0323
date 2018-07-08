591d86191f70f9aedc2929d781a8b90f028719aa
Bruna Thalenberg <bthalenberg@gmail.com>
2018-07-07 22:58:02 -0300

    linker and assembler

--- pr05/macas.c
+++ pr05/macas.c
@@ -1,52 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include "asm.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "error.h"
+#include "asm.h"
+#include "parser.h"
+#include "stable.h"
+#include "buffer.h"
 
-
-const char sufix[] = ".maco";
-
-//Recebe o nome do arquivo de entrada
-//gera o nome do arquivo de saida
-
-char* outputName(char *name){
-	int s, size, i=0;
-	char *out;
-	size = strlen(name);
-	out = malloc(sizeof(char)*(size+10));
-	while(i < size && name[i] != '.') {
-		out[i] = name[i];
-		i++;
+int main(int argc, char *argv[])
+{
+	if(argc == 1 || argc > 3) {
+		return 0;
 	}
-	s = i;
-	siz = strlen(sufix);
-	for(i = s; i < (s + size); i++)
-		out[i] = sufix[i - s];
-
-
-	return out;
-}
-
-
-/* Interprets command line, opens input and output files and calls assemble() to assemble code.
-Generates error messages where appropriate. */
-int main(int argc, char **argv) {
-    int i;
-	FILE *in, *out;
-	char *infile, *outfile;
-
-	for(i = 1; i < argc; i++) {
-		infile = argv[i];
-		outfile = outputName(infile);
-		in = fopen(infile, "r");
-		out = fopen(outfile, "w");
-		if(!assemble(infile, in, out)) 
-			die("error");
-		fclose(in);
-		fclose(out);
-	}	
-
-
-    return 0;
+	FILE *infile = fopen(argv[1],"r");
+	if(infile == NULL)
+		return 0;
+	FILE *outfile;
+	if(argc == 2) {
+		char *s = estrdup(argv[1]);
+		char *aux;
+		int tam = strlen(s);
+		for(int i = tam-1; i >= 0; i--) {
+			if(s[i] == '.') {
+				s[i] = '\0';
+				break;
+			}
+		}
+		aux = emalloc((tam+5)*sizeof(char));
+		strcpy(aux,s);
+		strcat(aux,".maco");
+		outfile = fopen(aux,"w");
+		free(s);
+		free(aux);
+	}
+	else {
+		outfile = fopen(argv[2],"w");
+	}
+	assemble(argv[1],infile,outfile);
+	fclose(infile); fclose(outfile);
+	return 0;
 }
--- pr05/maclk.c
+++ pr05/maclk.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "error.h"
+#include "parser.h"
+#include "asmtypes.h"
+#include "buffer.h"
+#include "stable.h"
+#include "asm.h"
+
 /* Reads many files with object code and links final program in assembly language.
 
 Errors checked:
@@ -7,5 +17,110 @@ Errors checked:
 */
 
 int main(int argc, char **argv) {
-    return 0;
+
+	if(argc <= 2) return 1;
+
+	Buffer *b = buffer_create();
+	SymbolTable extern_table = stable_create(); //SymbolTable que conterá os rótulos extern 
+	int n = argc - 2; //numero de códigos-objetos
+	int *acc = malloc((n+1) * sizeof(int)); //guardará a soma acumulada das linhas até o arquivo i
+	int main_pos = -1;  //guardará a posição do rótulo main 
+	FILE *out = fopen(argv[1], "w"); //arquivo de saida
+	FILE **in = emalloc((n+1) * sizeof(FILE*)); //n entradas
+	acc[0] = 0;
+
+	for(int i = 1; i <= n; i++) {
+		//Abre os arquivos de entrada
+		if((in[i] = fopen(argv[i+1],"r")) == NULL) {
+			die("ERRO file '%s' not found", argv[i+1]);
+		}
+		// Lê número de linhas
+		read_line(in[i], b);
+		// Calculamos soma acumulada
+		if(b->data[b->i-2] == '\n') b->data[b->i-2] = '\0';
+		int tam = atoi(b->data);
+		acc[i] = acc[i-1] + tam;
+	}
+
+	//Recebe os rótulos extern e põe na st com localização
+	// verifica erros de extern
+	for (int i = 1; i <= n; i++) {
+		while (read_line(in[i], b) && b->data[0] != 'B') {
+			if (b->data[b->i-2] == '\n') b->data[b->i-2] = '\0';
+			char *token = strtok(b->data," ");
+			char *label; 
+			int rel_pos;
+			token = strtok(NULL, " ");
+			label = estrdup(token);
+			token = strtok(NULL, " ");
+			rel_pos = atoi(token);
+			if (!strcmp(label,"main")) {
+				if (main_pos >= 0) {
+					die("EXTERN label main defined twice");
+				}
+				main_pos = acc[i-1] + rel_pos + 1;
+			}
+			InsertionResult res = stable_insert(extern_table, label);
+			if (res.new == 0) {
+				die("EXTERN label defined twice");
+			}
+			(*res.data).s = acc[i-1] + rel_pos + 1;
+			free(label);
+		}
+	}
+
+	//Se main_pos nao mudou de valor, main nao foi definida
+	if (main_pos == -1) die("EXTERN label 'main' never defined");
+	fprintf(out,"48%06x\n", main_pos);
+	int line = 0;
+
+	// Fazemos leitura linha a linha, calculando desvios necessários
+	for (int i = 1; i <= n; i++) {
+		while (read_line(in[i], b)) {
+			line++;
+			if (b->data[b->i-2] == '\n') b->data[b->i-2] = '\0';
+			if (b->data[0] == '*') {
+
+				char *token = strtok(b->data," *");
+				int opcode = atoi(token);
+				token = strtok(NULL, " *");
+
+				if (isdigit(token[0])) {
+					int reg = atoi(token);
+					token = strtok(NULL, " *");
+					EntryData *data;
+					data = stable_find(extern_table, token);
+					if (data == NULL) die("Unresolved symbol %s", token);
+					int dest = data->s, source = line;
+					if (dest >= source)
+						fprintf(out,"%02x%02x%04x\n", opcode, reg, dest - source);
+					else
+						fprintf(out,"%02x%02x%04x\n", opcode + 1, reg, source - dest);
+				}
+
+				else {
+					EntryData *data;
+					data = stable_find(extern_table, token);
+					if (data == NULL) die("Unresolved symbol '%s'", token);
+					int dest = data->s, source = line;
+					if (dest >= source)
+						fprintf(out, "%02x%06x\n", opcode, dest - source);
+					else
+						fprintf(out, "%02x%06x\n", opcode + 1, source - dest);
+				}
+			}
+
+			else fprintf(out,"%s\n",b->data);
+		}
+	}
+
+	free(acc);
+	fclose(out);
+	for (int i = 1; i <= n; i++) fclose(in[i]);
+	free(in);
+	stable_destroy(extern_table);
+	buffer_destroy(b);
+	return 0;
 }
+
+
